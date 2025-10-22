f7855db5cae3b52b2cd987072439624385c511ff
Anushka Dereddy <anushkareddyd@gmail.com>
2025-10-22 19:39:34 +0530

    cleanup: remove tracked venv and pycache

--- backend/.venv/app/core/passwords.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash_pw(p: str) -> str:
-    return pwd.hash(p)
-
-def verify_pw(p: str, h: str) -> bool:
-    return pwd.verify(p, h)
